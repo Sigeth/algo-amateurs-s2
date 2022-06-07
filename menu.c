8011fdb2f4cefcfe688f9a98d36d7e1a65231bb5
isen <103190340+Achecas@users.noreply.github.com>
2022-06-07 23:11:32 +0200

    menu

--- menu.c
+++ menu.c
@@ -1,5 +1,38 @@
-int main() {
-	printf("omg Bastient");
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
 
+int main(void) 
+{
+	int a=0;
+	printf("1 - Lancer simulation\n");
+	printf("2 - Reprendre simulation\n");
+	printf("3 - Sauvegarder simulation\n");
+	printf("4 - Charger simulation\n");
+	printf("5 - Quitter\n");
+	printf("que voulez vous faire ?");
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:
+			lancer_simulation();
+			break;
 
+		case 2:
+			reprendre()
+			break;
+
+		case 3:
+			sauvegarde()
+			break;
+
+		case 4:
+			charger()
+			break;
+
+		case 5:
+			quitter()
+			break;
+
+	}
 }
---
+++ menu.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+lancer_simulation();
+reprendre();
+sauvegarde();
+charger();
+quitter();
