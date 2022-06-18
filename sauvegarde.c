98037e80a088756e51e53bfd49e9bcc46d43689a
tseignour@gmail.com <52868615+Sigeth@users.noreply.github.com>
2022-06-18 11:43:39 +0200

    parcours de liste et ;

--- sauvegarde.c
+++ sauvegarde.c
@@ -4,48 +4,73 @@
 #include "sauvegarde.h"
 #include "moteur.h"
 #include <string.h>
-void initinfo(info a*){
-a->x=0;
-a->y=0;
-a->previousX=0;
-a->previousY=0;}
-void sauvergarde(char* nomdesauvegarde,Astre *ptTerre,Astre *ptLune,Astre *ptSoleil,Astre *ptMercure,Astre *ptVenus,Astre *ptMars,Astre *ptJupiter,Astre *ptSaturne,Astre *ptNeptune,Astre *ptUranus ){
-FILE* f=NULL;
-f=fopen(nomdesauvegarde,"a");
-if(f!=NULL){
-fputs(nomdesauvegarde,f);
-fclose(f);}}
-void nomdesauvegarde(){
-	printf("nom de la sauvegarde :");
-	char a[100];
-	scanf("%s",a);
-	FILE* f=NULL;
-	f=fopen("pastouche","a");
-	if(f!=NULL){
-	fputs(a,f);
-	fputs("\n",f);
-	fclose(f);}
-	FILE* fi=NULL;
-	fi=fopen(a,"w");
-	fclose(fi);
+
+void initinfo(info *a) {
+    a->x = 0;
+    a->y = 0;
+    a->previousX = 0;
+    a->previousY = 0;
 }
-void listesauvegarde(){
-char a[100];
-FILE* fi=fopen("pastouche","r");
-if(fi!=NULL){
-printf("voici la liste de vos sauvegarde :\n");
-while(fscanf(fi,"%s",a) != EOF){
-printf("%s \n",a);
+
+void sauvergarde(char *nomdesauvegarde, ElementAstre *ptElementAstreInitial) {
+    ElementAstre *ptElementAstreCourant = ptElementAstreInitial;
+    while (ptElementAstreCourant != NULL) {
+        Astre *ptAstre = ptElementAstreCourant->ptAstre;
+        if (ptAstre != NULL) {
+            printf("Bonjour, je suis %s\n", ptAstre->nom);
+        }
+
+        ptElementAstreCourant = ptElementAstreCourant->ptElementAstreSuivant;
+    }
+
+    FILE *f = NULL;
+    f = fopen(nomdesauvegarde, "a");
+    if (f != NULL) {
+        fputs(nomdesauvegarde, f);
+        fclose(f);
+    }
+
+    free(ptElementAstreCourant);
 }
-fclose(fi);}}
-void delsauvegarde(){
-char a[100];
-FILE* fi=fopen("pastouche","r");
-if(fi!=NULL){
-printf("voici la liste de vos sauvegarde :\n");
-while(fscanf(fi,"%s",a) != EOF){
-remove(a);}
-fclose(fi);
-remove("pastouche");
-}}
+
+void nomdesauvegarde() {
+    printf("nom de la sauvegarde :");
+    char a[100];
+    scanf("%s", a);
+    FILE *f = NULL;
+    f = fopen("pastouche", "a");
+    if (f != NULL) {
+        fputs(a, f);
+        fputs("\n", f);
+        fclose(f);
+    }
+    FILE *fi = NULL;
+    fi = fopen(a, "w");
+    fclose(fi);
+}
+
+void listesauvegarde() {
+    char a[100];
+    FILE *fi = fopen("pastouche", "r");
+    if (fi != NULL) {
+        printf("voici la liste de vos sauvegarde :\n");
+        while (fscanf(fi, "%s", a) != EOF) {
+            printf("%s \n", a);
+        }
+        fclose(fi);
+    }
+}
+
+void delsauvegarde() {
+    char a[100];
+    FILE *fi = fopen("pastouche", "r");
+    if (fi != NULL) {
+        printf("voici la liste de vos sauvegarde :\n");
+        while (fscanf(fi, "%s", a) != EOF) {
+            remove(a);
+        }
+        fclose(fi);
+        remove("pastouche");
+    }
+}
 
